e759cc315c7aa3bd2fa5b72b94d529e1af62ef3a
Jagathees0n <jagatheesanraj18@gmail.com>
2023-09-01 18:12:51 +0530

    after clearing the errors

--- server.js
+++ server.js
@@ -2,15 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
-import User from './models/User';
+import User from './models/User.js';
+import bcrypt from 'bcrypt';
+import cors from 'cors';
 
-await mongoose.connect('mongodb://localhost:27017/auth', 
+await mongoose.connect('mongodb://127.0.0.1:27017/auth', 
 {useNewUrlParser:true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.log);
 
 const app = express();
 
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true, // Enable credentials (cookies, headers)
+  };
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(bodyParser.json({extended:true}));
 
@@ -20,7 +28,17 @@ app.get('/',(req, res)=>{
 
 app.post('/register', (req, res)=>{
     const {email, password} = req.body;
-    const user = User
-})
+    const hashedPassword = bcrypt.hashSync(password,10);
+    const user = new User({
+        email,
+        password : hashedPassword
+    });
+    user.save().then(userInfo => {
+        console.log(userInfo);
+    })
+});
 
-app.listen(4000);
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
